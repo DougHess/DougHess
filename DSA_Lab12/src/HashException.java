ea42708c614c116294e277982b0a7f761efabe8a
DougHess <73005473+DougHess@users.noreply.github.com>
2021-04-06 10:55:23 -0400

    Delete DSA_Lab12 directory

--- DSA_Lab12/src/HashException.java
+++
@@ -1,6 +0,0 @@
-
-public class HashException extends RuntimeException {
-  public HashException(String s) {
-    super(s);
-  }  // end constructor
-} // end HashException
--- DSA_Lab12/src/Rank.java
+++
@@ -1,9 +0,0 @@
-
-public class Rank {
-
-	public int rank(char c) {
-		c = Character.toUpperCase(c);
-		int rank = c-'A'+1;
-		return rank;
-	}
-}
